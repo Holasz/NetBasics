5a0ee9fb44e4bf5492c0b834d13fb6adfb187724
Holasz <holaszmonika@yahoo.com>
2016-10-23 16:48:42 +0300

    commit

---
+++ Mercedes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial
+{
+    // mostenim clasa AbstractCar
+    public class  Mercedes : AbstractCar
+    {
+        public string Description;
+        public int Price;
+        private string _description;
+        private int _price;
+        protected string type;
+        protected int TVA;
+        internal string color;
+        internal int speed;   
+        // constructor
+        public Mercedes()
+        {
+            Description = "E Class";
+            Price =67000;
+            _description = "new description";
+            _price = 60000;
+            type = "C";
+            TVA = 20;
+            color = "blue";
+            speed = 4;
+
+        }
+    }
+
+   
+
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial
+{
+    class Program
+    {
+        struct EngineStruct
+        {
+        }
+
+        static void Main(string[] args)
+        {
+            //instantiam clasa Mercedes  
+            Mercedes mercedes = new Mercedes();
+            EngineStruct engine = new EngineStruct();
+            ICar car = new Mercedes();
+            AbstractCar car2 = new Mercedes();
+            //AbstractCar aCar = new AbstractCar();
+            Console.WriteLine("This is my first C# project!"); // afisam un mesaj in consola 
+            Console.WriteLine("Mercedes is " + mercedes.Description);
+            Console.WriteLine("Mercedes costs " + mercedes.Price);
+
+           // Console.WriteLine("Mercedes is " + mercedes._price);
+            //Console.WriteLine(mercedes.type); ii protected si nu poate fi apelat aici
+            Console.WriteLine("mercedes color is " + mercedes.color);
+            Console.ReadKey();
+        }
+    }
+}
