f9aec4868fbf5d9ef2b6bb6d4c9950a41f53b5a2
Holasz <holaszmonika@yahoo.com>
2016-10-23 16:39:54 +0300

    first commit of the project

---
+++ AbstractCar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial
+{
+    // mostenim interfata ICar 
+    public abstract class AbstractCar : ICar
+    {
+        public AbstractCar()
+        {
+            // TODO: Complete member initialization
+        }
+    }
+}
