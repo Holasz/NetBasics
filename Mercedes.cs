90435a42418baebe14d2113441bfa402a4628668
Holasz <holaszmonika@yahoo.com>
2016-11-06 15:07:44 +0200

    Curs 3
    Curs 3

--- Mercedes.cs
+++ Mercedes.cs
@@ -30,8 +30,41 @@ namespace Tutorial
             speed = 4;
 
         }
+        private int counter;
+
+        public void PrintMercedesLogo()
+        {
+            counter++;
+            if (counter == 3) return;
+            Console.WriteLine("This is from Mercedes");
+            PrintMercedesLogo();
+        }
+
+        public void PrintNewMercedes(int c)
+        {
+            if (c == 3) return;
+            Console.WriteLine("Mercedes No = " + c);
+        }
+        public static int staticcounter;
+
+        public static void StartEngine()
+        {
+            Console.WriteLine("Engine Started");
+        }
+
+        public double ShowRemainingNoOfKM(double noOfFuelLiters, double Consumption)
+        {
+            //Console.WriteLine("Remaining km:" + noOfFuelLiters / Consumption * 100);
+            double result = 0;
+            result = noOfFuelLiters / Consumption * 100;
+            return result;
+        }
+
+        internal void ShowRemainingNoOFKM(int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
     }
 
-   
-
 }
+
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,24 +11,68 @@ namespace Tutorial
     {
         struct EngineStruct
         {
-        }
 
-        static void Main(string[] args)
-        {
-            //instantiam clasa Mercedes  
-            Mercedes mercedes = new Mercedes();
-            EngineStruct engine = new EngineStruct();
-            ICar car = new Mercedes();
-            AbstractCar car2 = new Mercedes();
-            //AbstractCar aCar = new AbstractCar();
-            Console.WriteLine("This is my first C# project!"); // afisam un mesaj in consola 
-            Console.WriteLine("Mercedes is " + mercedes.Description);
-            Console.WriteLine("Mercedes costs " + mercedes.Price);
+            static void Main(string[] args)
+            {
+                //instantiam clasa Mercedes  
+                Mercedes mercedes = new Mercedes();
+                EngineStruct engine = new EngineStruct();
+                ICar car = new Mercedes();
+                AbstractCar car2 = new Mercedes();
+                //AbstractCar aCar = new AbstractCar();
+                Console.WriteLine("This is my first C# project!"); // afisam un mesaj in consola 
+                Console.WriteLine("Mercedes is " + mercedes.Description);
+                Console.WriteLine("Mercedes costs " + mercedes.Price);
 
-           // Console.WriteLine("Mercedes is " + mercedes._price);
-            //Console.WriteLine(mercedes.type); ii protected si nu poate fi apelat aici
-            Console.WriteLine("mercedes color is " + mercedes.color);
-            Console.ReadKey();
+                // Console.WriteLine("Mercedes is " + mercedes._price);
+                //Console.WriteLine(mercedes.type); ii protected si nu poate fi apelat aici
+                Console.WriteLine("mercedes color is " + mercedes.color);
+                Console.ReadKey();
+
+                Mercedes m1 = new Mercedes();
+                //m.PrintMercedesLogo();
+                //m1.PrintNewMercedes(Mercedes.staticcounter++);
+                //Mercedes m2 = new Mercedes();
+                //Mercedes m3 = new Mercedes();
+                //Mercedes m4 = new Mercedes();
+                //m2.PrintNewMercedes(Mercedes.staticcounter++);
+                //m3.PrintNewMercedes(Mercedes.staticcounter++);
+                //m4.PrintNewMercedes(Mercedes.staticcounter++);
+
+                //Mercedes.StartEngine();
+
+                Mercedes asa = new Mercedes();
+                asa.ShowRemainingNoOFKM(10, 7);
+
+                double numberOfKilometerLeft;
+                numberOfKilometerLeft = asa.ShowRemainingNoOfKM(10, 7);
+                Console.WriteLine("Numarul de km ramasi: " + numberOfKilometerLeft);
+                Console.ReadKey();
+
+
+            }
+
+            static int counter;
+
+            static void PrintLogo()
+            {
+                Console.WriteLine("I am learning C#");
+                Console.WriteLine("C#is great");
+                PrintLogo2();
+            }
+            static void PrintLogo(int i)
+            {
+                Console.WriteLine("I am another printlogo");
+            }
+            static void PrintLogo2()
+            {
+                counter++;
+                if (counter == 3) return;
+                Console.WriteLine("PrintLogo2");
+                PrintLogo();
+            }
         }
     }
 }
+
+
